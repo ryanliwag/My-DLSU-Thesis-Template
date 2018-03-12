44a472e9491ddf5a83aea17932c4e809f04d67e5
ryanliwag <rjhontomin@gmail.com>
2018-03-12 23:39:37 +0800

    minor addition

--- CtCI/String_Arrays.py
+++ CtCI/String_Arrays.py
@@ -85,3 +85,22 @@ def is_one_away(first: str, other: str) -> bool:
 
     remain_first, remain_other = skip(i + 1)
     return first[remain_first:] == other[remain_other:]
+
+def compress(string):
+    res = ""
+    count = 1
+    res += string[0]
+    
+    for i in range(len(string)-1):
+        if(string[i] == string[i+1]):
+            count+=1
+        else:
+            if(count > 1):
+                #Ignore if no repeats
+                res += str(count)
+            res += string[i+1]
+            count = 1
+    #print last one
+    if(count > 1):
+        res += str(count)
+    return res
